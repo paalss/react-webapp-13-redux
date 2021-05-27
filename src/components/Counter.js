2420be8189735fc528809e673209c6ad2e689bb5
paalss <paalss94@gmail.com>
2021-05-27 18:15:21 +0200

    bruker redux toolkit for å gjøre litt heavy-lifting

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "../store/index";
 import classes from "./Counter.module.css";
+
 
 const Counter = () => {
   const dispatch = useDispatch();
@@ -7,19 +9,19 @@ const Counter = () => {
   const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(5));
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
